d7ef38133e0e9fb59ac26a65bd8ed6fb307f1bf3
ptabasso <pejman.tabassomi@appdynamics.com>
2016-10-05 12:20:32 +0200

    Change in message

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,6 @@
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Executable jar example");
+        System.out.println("Exe jar example");
     }
 }
